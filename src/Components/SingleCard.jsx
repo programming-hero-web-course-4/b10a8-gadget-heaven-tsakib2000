d965179439514d23589f57c61c641c43afc50890
tsakib2000 <isakib49@gmail.com>
2024-11-04 00:55:26 +0600

    fixed footer

--- src/Components/SingleCard.jsx
+++ src/Components/SingleCard.jsx
@@ -6,7 +6,7 @@ const SingleCard = ({ gadget }) => {
     price,
   } = gadget;
   return<div className="card bg-base-100 h-fit shadow-xl">
-  <figure className="px-10 pt-10  h-64">
+  <figure className="px-10 pt-10 py-4  h-64">
     <img
       src={product_image}
       alt={product_title}
@@ -16,7 +16,7 @@ const SingleCard = ({ gadget }) => {
     <h2 className="card-title">{product_title}</h2>
     <p>Price:{price}</p>
     <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn text-violet-600 bg-white border-violet-600 rounded-3xl" >View Details</button>
     </div>
   </div>
 </div>;
